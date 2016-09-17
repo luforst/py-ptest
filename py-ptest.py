95039740272c68a1e64c679da81933443290a82a
luforst <luforst@users.noreply.github.com>
2016-09-17 10:37:11 +0200

    Initial commit

---
+++ py-ptest.py
@@ -0,0 +1,23 @@
+#Title: py-primality-test
+#Author: Lukas Forst
+#License: MIT license
+
+from math import sqrt
+
+def ptest_bf(x): #bf stands for brute force
+    i = 2
+    if x < 2: #only positive integers are possible, 1 is neither prime nor non-prime
+        return False
+    while i <= round(sqrt(x)):
+        if x%i == 0:
+            return False
+        i += 1
+    return True
+
+if __name__ == "__main__":
+    numbers =[1111, 1113, 1131, 1311, 3111, 3311, 3131, 3113, 1331, 1313, 1133, 1333, 3133, 3313, 3331, 3333]
+    primes = []
+    for n in numbers:
+        res = ptest_bf(n)
+        print("{0}: {1}".format(n, res))
+
